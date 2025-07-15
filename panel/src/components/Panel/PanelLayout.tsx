070e5dd6e9c8304f0eb6482a1f807351535e728c
ivangastondamb@gmail.com <ivangastondamb@gmail.com>
2025-07-15 18:57:01 -0300

    Mini fix v2

--- panel/src/components/Panel/PanelLayout.tsx
+++ panel/src/components/Panel/PanelLayout.tsx
@@ -1,24 +1,18 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./panel.css";
 
 const PanelLayout = () => {
-  interface User {
-    rol: string;
-  }
 
-  const [rol, setRol] = useState<User | null>(null);
-  const navigate = useNavigate();
+const navigate = useNavigate();
 
 useEffect(() => {
   const rol = localStorage.getItem("rol");
   if (!rol) {
     navigate("/login");
-  } else {
-    setRol({ rol }); // pasamos un objeto con el rol
-  }
-}, []);
+  } 
+});
 
   return (
     <div className="panel-layout">
